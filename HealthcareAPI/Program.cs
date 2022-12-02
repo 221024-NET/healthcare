ee6ff3071852b3af5b9a048506f3c3b244fcc083
MasonCathorall <masoncathorallrev@gmail.com>
2022-12-02 15:54:53 -0500

    added the sql database

--- HealthcareAPI/Program.cs
+++ HealthcareAPI/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connValue = builder.Configuration.GetValue<string>("ConnectionStrings:Azure");
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
